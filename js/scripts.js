ca3e7e210b0438f4735b16fe1ad00a708dc65c71
Kevin Rombouts <mail@kevinrombouts.nl>
2021-06-21 14:02:37 +0200

    Fixed menu on mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,31 @@
 //Haalt het menu op
-var oReq = new XMLHttpRequest();
-var sidebar = document.querySelector('#header');
-oReq.addEventListener("load", reqListener);
-oReq.open("GET", "./nav.html");
-oReq.send();
+ var oReq = new XMLHttpRequest();
+ var sidebar = document.querySelector('#header');
+ oReq.addEventListener("load", reqListener);
+ oReq.open("GET", "./nav.html");
+ oReq.send();
 
-function reqListener () {
- sidebar.innerHTML = this.responseText;
-};
+ function reqListener () {
+  sidebar.innerHTML = this.responseText;
+ };
 
-//Maak div's clickable
-$(".item").click(function(){
- window.location=$(this).find("a").attr("href");
- return false;
-});
+ //Mobile menu
+ $("#header").on("click", "#mobMenuIcon", function(){
+  console.log("Click on icon");
+  $("i").toggleClass("fa-times");
+  $("#navbar").toggleClass("active");
+  $("#app").toggleClass("menuOpen");
+  $("#content").toggleClass("menuOpen");
+ });
+
+ //Maak div's clickable
+ $(".item").click(function(){
+  window.location=$(this).find("a").attr("href");
+  return false;
+ });
+
+ //Download notificatie
+ function downloadNotify(message, wait) {
+  $("#notificationBox p").text(message);
+  
+ };
